98c0f9ec031d09d0d645c02a0b1c8e1e6f1eace5
加藤　優奈 <yuvernacrew@gmail.com>
2019-01-28 23:53:22 +0900

    update ジェスチャー判定とAppleScript結びつけ

--- src/isHideUI.kt
+++ src/isHideUI.kt
@@ -30,22 +30,22 @@ class SampleKtListener : Listener() {
     override fun onFrame(controller: Controller) {
         // Get the most recent frame and report some basic information
         val frame = controller.frame()
-        println(
-            "Frame id: " + frame.id()
-                    + ", timestamp: " + frame.timestamp()
-                    + ", hands: " + frame.hands().count()
-                    + ", fingers: " + frame.fingers().count()
-                    + ", tools: " + frame.tools().count()
-                    + ", gestures " + frame.gestures().count()
-        )
+//        println(
+//            "Frame id: " + frame.id()
+//                    + ", timestamp: " + frame.timestamp()
+//                    + ", hands: " + frame.hands().count()
+//                    + ", fingers: " + frame.fingers().count()
+//                    + ", tools: " + frame.tools().count()
+//                    + ", gestures " + frame.gestures().count()
+//        )
 
         //Get hands
         for (hand in frame.hands()) {
             val handType = if (hand.isLeft) "Left hand" else "Right hand"
-            println(
-                "  " + handType + ", id: " + hand.id()
-                        + ", palm position: " + hand.palmPosition()
-            )
+//            println(
+//                "  " + handType + ", id: " + hand.id()
+//                        + ", palm position: " + hand.palmPosition()
+//            )
 
 
             // Get the hand's normal vector and direction
@@ -53,39 +53,39 @@ class SampleKtListener : Listener() {
             val direction = hand.direction()
 
             // Calculate the hand's pitch, roll, and yaw angles
-            println(
-                "  pitch: " + Math.toDegrees(direction.pitch().toDouble()) + " degrees, "
-                        + "roll: " + Math.toDegrees(normal.roll().toDouble()) + " degrees, "
-                        + "yaw: " + Math.toDegrees(direction.yaw().toDouble()) + " degrees"
-            )
+//            println(
+//                "  pitch: " + Math.toDegrees(direction.pitch().toDouble()) + " degrees, "
+//                        + "roll: " + Math.toDegrees(normal.roll().toDouble()) + " degrees, "
+//                        + "yaw: " + Math.toDegrees(direction.yaw().toDouble()) + " degrees"
+//            )
 
 
             // Get arm bone
             val arm = hand.arm()
-            println(
-                "  Arm direction: " + arm.direction()
-                        + ", wrist position: " + arm.wristPosition()
-                        + ", elbow position: " + arm.elbowPosition()
-            )
+//            println(
+//                "  Arm direction: " + arm.direction()
+//                        + ", wrist position: " + arm.wristPosition()
+//                        + ", elbow position: " + arm.elbowPosition()
+//            )
 
             // Get fingers
             for (finger in hand.fingers()) {
-                println(
-                    "    " + finger.type() + ", id: " + finger.id()
-                            + ", length: " + finger.length()
-                            + "mm, width: " + finger.width() + "mm"
-                )
+//                println(
+//                    "    " + finger.type() + ", id: " + finger.id()
+//                            + ", length: " + finger.length()
+//                            + "mm, width: " + finger.width() + "mm"
+//                )
 
 
                 //Get Bones
                 for (boneType in Bone.Type.values()) {
                     val bone = finger.bone(boneType)
-                    println(
-                        "      " + bone.type()
-                                + " bone, start: " + bone.prevJoint()
-                                + ", end: " + bone.nextJoint()
-                                + ", direction: " + bone.direction()
-                    )
+//                    println(
+//                        "      " + bone.type()
+//                                + " bone, start: " + bone.prevJoint()
+//                                + ", end: " + bone.nextJoint()
+//                                + ", direction: " + bone.direction()
+//                    )
 
                 }
             }
@@ -93,11 +93,11 @@ class SampleKtListener : Listener() {
 
         // Get tools
         for (tool in frame.tools()) {
-            println(
-                "  Tool id: " + tool.id()
-                        + ", position: " + tool.tipPosition()
-                        + ", direction: " + tool.direction()
-            )
+//            println(
+//                "  Tool id: " + tool.id()
+//                        + ", position: " + tool.tipPosition()
+//                        + ", direction: " + tool.direction()
+//            )
         }
 
         val gestures = frame.gestures()
@@ -136,39 +136,44 @@ class SampleKtListener : Listener() {
 
                 Gesture.Type.TYPE_SWIPE -> {
                     val swipe = SwipeGesture(gesture)
-//                    println(
-//                        "  Swipe id: " + swipe.id()
-//                                + ", " + swipe.state()
-//                                + ", position: " + swipe.position()
-//                                + ", direction: " + swipe.direction()
-//                                + ", speed: " + swipe.speed()
-//                    )
                     println(
-                        "swipeしたよ"
+                        "Swipe id: " + swipe.id()
+                                + ", " + swipe.state()
+                                + ", position: " + swipe.position()
+                                + ", direction: " + swipe.direction()
+                                + ", speed: " + swipe.speed()
+                                + "direction x :"+ swipe.direction().x
+                                + "direction y :"+ swipe.direction().y
                     )
+
                     val runtime = Runtime.getRuntime()
-                    val args = arrayOf("osascript", "-e", "tell app \"iTunes\" to play")
+                    val args =
+                        if (swipe.direction().x >0) {
+                            arrayOf("osascript", "-e", "tell app \"iTunes\" back track")
+                        } else {
+                            arrayOf("osascript", "-e", "tell app \"iTunes\" to next track")
+                        }
+                    println(args)
                     val process = runtime.exec(args)
                 }
 
                 Gesture.Type.TYPE_SCREEN_TAP -> {
                     val screenTap = ScreenTapGesture(gesture)
-//                    println(
-//                        "  Screen Tap id: " + screenTap.id()
-//                                + ", " + screenTap.state()
-//                                + ", position: " + screenTap.position()
-//                                + ", direction: " + screenTap.direction()
-//                    )
+//
                 }
 
                 Gesture.Type.TYPE_KEY_TAP -> {
                     val keyTap = KeyTapGesture(gesture)
-//                    println(
-//                        "  Key Tap id: " + keyTap.id()
-//                                + ", " + keyTap.state()
-//                                + ", position: " + keyTap.position()
-//                                + ", direction: " + keyTap.direction()
-//                    )
+                    println(
+                        "  Key Tap id: " + keyTap.id()
+                                + ", " + keyTap.state()
+                                + ", position: " + keyTap.position()
+                                + ", direction: " + keyTap.direction()
+                    )
+
+                    val runtimeTap = Runtime.getRuntime()
+                    val argsTap = arrayOf("osascript", "-e", "tell app \"iTunes\" to playpause")
+                    val processTap = runtimeTap.exec(argsTap)
                 }
 
                 else -> println("Unknown gesture type.")
