fa0b7aeeea6e11e1a27288f144caa6c58fd48279
ShuzoTakahashi <shuzo.create@gmail.com>
2019-02-18 18:08:13 +0900

    update: GestureListenerにCoroutineScopeを実装
    Signed-off-by: ShuzoTakahashi <shuzo.create@gmail.com>

---
+++ src/GestureListener.kt
@@ -0,0 +1,101 @@
+import java.io.IOException
+import com.leapmotion.leap.*
+import communication.ComTcpClient
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlin.coroutines.CoroutineContext
+
+internal class GestureListener(private val connection: ComTcpClient) : Listener(), CoroutineScope {
+
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = job
+
+    override fun onInit(controller: Controller) = println("Initialized")
+
+    override fun onConnect(controller: Controller) {
+        println("Connected")
+        controller.apply {
+            enableGesture(Gesture.Type.TYPE_SWIPE)
+            enableGesture(Gesture.Type.TYPE_SCREEN_TAP)
+        }
+    }
+
+    override fun onDisconnect(controller: Controller) = println("Disconnected")
+
+    override fun onExit(controller: Controller) = println("Exited")
+
+    override fun onFrame(controller: Controller) {
+        if (!connection.isConnected) return
+        launch {
+            controller.frame().also { frame ->
+                frame.gestures().also { gestures ->
+                    gestures.forEach { gesture ->
+                        when (gesture.type()) {
+                            Gesture.Type.TYPE_SWIPE -> {
+                                println("タップ")
+
+                                val swipe = SwipeGesture(gesture)
+                                var lightStateSwipe = false
+
+                                connection.getIO { output, _ ->
+                                    output.write("on".toByteArray())
+                                }
+                            }
+
+                            Gesture.Type.TYPE_SCREEN_TAP -> {
+                                println("タップ")
+                                val screenTap = ScreenTapGesture(gesture)
+
+                                connection.getIO { output, _ ->
+                                    output.write("on".toByteArray())
+                                }
+                            }
+                            else -> println("Unknown gesture type.")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+fun main() {
+
+    val channel = Channel<Enum<ComTcpClient.ComState>>().also { channel ->
+        GlobalScope.launch(Dispatchers.Default) {
+            when (channel.receive()) {
+
+                ComTcpClient.ComState.MSG_IOEXCEPTION -> {
+                    // TODO : socketのclose処理
+                }
+
+                ComTcpClient.ComState.MSG_CONNECTION_FAILED -> {
+                    // TODO : socketのclose処理
+                }
+
+                ComTcpClient.ComState.MSG_CONNECTION_SUCCESS -> {
+                    print("らずぱいせつぞく")
+                }
+            }
+        }
+    }
+
+    val connection = ComTcpClient("172.20.10.4", 55555, channel)
+    connection.connect()
+
+    val controller = Controller()
+    val listener = GestureListener(connection)
+
+    controller.addListener(listener)
+
+    println("Press Enter to quit... ")
+    try {
+        System.`in`.read()
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+
+    controller.removeListener(listener)
+}
