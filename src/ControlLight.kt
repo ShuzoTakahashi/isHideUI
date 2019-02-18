f621e246845949ca0501de98507edefcb6374f3c
ShuzoTakahashi <shuzo.create@gmail.com>
2019-02-18 16:56:24 +0900

    update: functionalに

--- src/ControlLight.kt
+++ src/ControlLight.kt
@@ -5,32 +5,10 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.launch
-import communication.ComTcpClient.ComState.*
 
-class ControlLight : Listener() {
+class ControlLight(val channel: Channel<Enum<ComTcpClient.ComState>>) : Listener() {
 
-    private val channel by lazy {
-        Channel<Enum<ComTcpClient.ComState>>().also { channel ->
-            GlobalScope.launch(Dispatchers.Default) {
-                when (channel.receive()) {
-
-                    MSG_IOEXCEPTION -> {
-                        // TODO : socketのclose処理
-                    }
-
-                    MSG_CONNECTION_FAILED -> {
-                        // TODO : socketのclose処理
-                    }
-
-                    MSG_CONNECTION_SUCCESS -> {
-                        print("らずぱいせつぞく")
-                    }
-                }
-            }
-        }
-    }
     private lateinit var connection: ComTcpClient
-
 
     override fun onInit(controller: Controller) = println("Initialized")
 
@@ -46,58 +24,62 @@ class ControlLight : Listener() {
     override fun onExit(controller: Controller) = println("Exited")
 
     override fun onFrame(controller: Controller) {
-        val frame = controller.frame()
 
+        controller.frame().also { frame ->
+            frame.gestures().also { gestures ->
+                gestures.forEach { gesture ->
+                    when (gesture.type()) {
+                        Gesture.Type.TYPE_SWIPE -> {
+                            val swipe = SwipeGesture(gesture)
+                            var lightStateSwipe = false
+                            println("スワイプ")
 
-        val gestures = frame.gestures()
-        for (i in 0 until gestures.count()) {
-            val gesture = gestures.get(i)
+                            connection.getIO { output, _ ->
+                                output.write("on".toByteArray())
+                            }
+                        }
 
-            when (gesture.type()) {
+                        Gesture.Type.TYPE_SCREEN_TAP -> {
+                            println("タップ")
+                            val screenTap = ScreenTapGesture(gesture)
 
-                Gesture.Type.TYPE_SWIPE -> {
-                    println("スワイプ")
-                    val swipe = SwipeGesture(gesture)
-                    var lightStateSwipe = false
-
-                    connection.getIO { output, _ ->
-                        output.write("on".toByteArray())
+                            connection.getIO { output, _ ->
+                                output.write("on".toByteArray())
+                            }
+                        }
+                        else -> println("Unknown gesture type.")
                     }
                 }
-
-                Gesture.Type.TYPE_SCREEN_TAP -> {
-                    println("タップ")
-                    val screenTap = ScreenTapGesture(gesture)
-
-                    connection.getIO { output, _ ->
-                        output.write("on".toByteArray())
-                    }
-                }
-
-                Gesture.Type.TYPE_KEY_TAP -> {
-                    println("key tap！")
-                    val keyTap = KeyTapGesture(gesture)
-                    println(
-                        "  Key Tap id: " + keyTap.id()
-                                + ", " + keyTap.state()
-                                + ", position: " + keyTap.position()
-                                + ", direction: " + keyTap.direction()
-                    )
-                }
-
-                else -> println("Unknown gesture type.")
             }
-        }
-
-        if (!frame.hands().isEmpty || !gestures.isEmpty) {
-            println()
         }
     }
 }
 
 
 fun main() {
-    val listener = ControlLight()
+
+    val channel by lazy {
+        Channel<Enum<ComTcpClient.ComState>>().also { channel ->
+            GlobalScope.launch(Dispatchers.Default) {
+                when (channel.receive()) {
+
+                    ComTcpClient.ComState.MSG_IOEXCEPTION -> {
+                        // TODO : socketのclose処理
+                    }
+
+                    ComTcpClient.ComState.MSG_CONNECTION_FAILED -> {
+                        // TODO : socketのclose処理
+                    }
+
+                    ComTcpClient.ComState.MSG_CONNECTION_SUCCESS -> {
+                        print("らずぱいせつぞく")
+                    }
+                }
+            }
+        }
+    }
+
+    val listener = ControlLight(channel)
     val controller = Controller()
 
     controller.addListener(listener)
